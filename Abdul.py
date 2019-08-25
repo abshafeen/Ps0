f0de0faa6d83c9d10628bf037e7be1955a1eb526
abshafeen <54500420+abshafeen@users.noreply.github.com>
2019-08-25 14:39:50 +0100

    Add files via upload

---
+++ Abdul.py
@@ -0,0 +1,10 @@
+#Ashafa Abdullahi
+#25/08/2019
+#program to print x is raised to the power of y and print log base 2 of x
+import math
+x = int(input("enter a number: "))
+y = int(input("enter another number: "))
+power = x**y
+print(x, "raised to the power of ",y, "is ",power)
+log =(x)=1
+print("log base 2 of ",x,"is ",log)
